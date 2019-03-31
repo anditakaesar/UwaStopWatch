a4f26d46ed3186a4c08bce042ad0617be685d7ee
anditakaesar <anditakaesar@gmail.com>
2019-03-31 11:16:43 +0700

    add files

---
+++ UwaStopWatch/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace UwaStopWatch
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtClock = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timerMain = new System.Windows.Forms.Timer(this.components);
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtClock
+            // 
+            this.txtClock.Enabled = false;
+            this.txtClock.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtClock.Location = new System.Drawing.Point(12, 12);
+            this.txtClock.Name = "txtClock";
+            this.txtClock.Size = new System.Drawing.Size(235, 39);
+            this.txtClock.TabIndex = 0;
+            this.txtClock.Text = "000:00";
+            this.txtClock.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 57);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "&START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timerMain
+            // 
+            this.timerMain.Interval = 1000;
+            this.timerMain.Tick += new System.EventHandler(this.timerMain_Tick);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(172, 57);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "&RESET";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 94);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtClock);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Uwa Stopwatch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtClock;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timerMain;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ UwaStopWatch/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UwaStopWatch
+{
+    public partial class MainForm : Form
+    {
+
+        private static int seconds = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (timerMain.Enabled)
+            {
+                timerMain.Stop();
+                btnStart.Text = "&START";
+            }
+            else
+            {
+                timerMain.Start();
+                btnStart.Text = "&STOP";
+            }
+            
+        }
+
+        private void timerMain_Tick(object sender, EventArgs e)
+        {
+            seconds += 1;
+            txtClock.Text = getTimeString(seconds);
+        }
+
+        private string getTimeString(int seconds)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int min = seconds / 60;
+            seconds = seconds % 60;
+
+            sb.Append(min.ToString("D3"))
+                .Append(":")
+                .Append(seconds.ToString("D2"));
+
+            return sb.ToString();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            timerMain.Stop();
+            seconds = 0;
+            txtClock.Text = getTimeString(seconds);
+        }
+    }
+}
